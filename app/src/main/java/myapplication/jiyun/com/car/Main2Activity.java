05c1f1f2a7dcbd503e81b2b709248ab362035523
Administrator <844234679@qq.com>
2018-04-01 19:55:54 +0800

    Initial commit

---
+++ app/src/main/java/myapplication/jiyun/com/car/Main2Activity.java
@@ -0,0 +1,35 @@
+package myapplication.jiyun.com.car;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        initView();
+    }
+
+    private void initView() {
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
